333768e6d629eb2e8d7f85d53eec9c16a139ecbc
Matheus Kaúlly <kaully.santos@gmail.com>
2023-11-13 23:58:46 -0300

    calculatr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,123 @@
-import Image from 'next/image'
+'use client'
+
+import { Github, Linkedin } from 'lucide-react'
+import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Home() {
+  const calculadoraItens = [
+    { value: '1', off: false },
+    { value: '2', off: false },
+    { value: '3', off: false },
+    { value: '+', expression: true, off: false },
+    { value: '4', off: false },
+    { value: '5', off: false },
+    { value: '6', off: false },
+    { value: '-', expression: true, off: false },
+    { value: '7', off: false },
+    { value: '8', off: false },
+    { value: '9', off: false },
+    { value: '*', expression: true, off: false },
+    { value: '0', off: false },
+    { value: '.', off: true },
+    { value: '=', expression: true, off: false },
+    { value: '/', expression: true, off: false },
+    { value: 'C', expression: true, off: false },
+    { value: '%', expression: true, off: false },
+  ]
+
+  const [operation, setOperation] = useState('')
+
+  function clear() {
+    setOperation('')
+  }
+
+  function result() {
+    try {
+      setOperation(eval(operation).toString())
+    } catch (error) {
+      setOperation('falha ao calcular')
+    }
+  }
+
+  function handleButtonClick(value: string) {
+    setOperation((prevExpression) => prevExpression + value)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="flex items-center justify-center h-screen mx-auto flex-col gap-4">
+      <div className="max-w-sm bg-gray-800 shadow-lg rounded overflow-hidden ">
+        <div className="px-6 pt-12">
+          <div className="p-5 bg-gray-900 rounded-lg flex justify-between gap-2">
+            <p className="font-bold flex items-center justify-center text-xl">
+              C
+            </p>
+            <input
+              className="font-bold text-xl bg-gray-900 flex text-right animate-pulse placeholder:opacity-20 "
+              placeholder="00000000000000000000"
+              value={operation}
+              type="text"
+              readOnly
             />
-          </a>
+          </div>
+        </div>
+        <div className="grid grid-cols-4 gap-4 p-8 bg-gray-">
+          {calculadoraItens.map((item) => {
+            return (
+              <button
+                disabled={item.off ?? true}
+                onClick={() => {
+                  if (item.value === 'C') {
+                    clear()
+                  } else if (item.value === '=') {
+                    result()
+                  } else handleButtonClick(item.value)
+                }}
+                key={item.value}
+                className={`h-12 border shadow transition-all border-gray-300 p-4 flex justify-center items-center rounded ${
+                  item.expression
+                    ? 'bg-purple-500 hover:bg-purple-600'
+                    : 'bg-gray-700 hover:bg-gray-800'
+                } ${
+                  item.off
+                    ? 'cursor-not-allowed hover:bg-red-500'
+                    : 'cursor-pointer'
+                }`}
+              >
+                {item.value}
+              </button>
+            )
+          })}
+          <div className="col-span-2 items-center flex gap-2 justify-end pr-4">
+            <Link href={'https://github.com/matheuskaully'} target="_blank">
+              <Github
+                size={25}
+                className="hover:text-purple-500 transition-colors"
+              />
+            </Link>
+
+            <Link
+              href={'https://www.linkedin.com/in/matheuskaully/'}
+              target="_blank"
+            >
+              <Linkedin
+                size={25}
+                className="hover:text-purple-500 transition-colors"
+              />
+            </Link>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="px-2 text-sm ">
+        <p>
+          Codado com 💖 por{' '}
+          <Link
+            className="items-center hover:text-purple-500 transition-colors"
+            href={'https://github.com/matheuskaully'}
+          >
+            Matheus Kaúlly
+          </Link>
+        </p>
       </div>
     </main>
   )
